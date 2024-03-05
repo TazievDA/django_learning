d226d74be5c2007e2cc443659f79d36dd95e2fa1
Denis Taziev <taziev.da@gmail.com>
2024-03-06 00:31:49 +0500

    Added homework 1.2 (pagination)

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,8 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+import csv
+import os
 
 
 def index(request):
@@ -9,9 +12,14 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
-    context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
-    }
+    with open(f'{os.getcwd()}\data-398-2018-08-30.csv', encoding='UTF-8') as f:
+        reader = csv.DictReader(f)
+        data = list(reader)
+        paginator = Paginator(data, 10)
+        page_number = int(request.GET.get('page', 1))
+        page = paginator.get_page(page_number)
+        context = {
+            'page': page,
+            'bus_stations': page,
+        }
     return render(request, 'stations/index.html', context)
